125b1204a849d0e1b2f8b5204d85c7db82913302
andarutr <andarutr@gmail.com>
2024-05-25 16:29:26 +0700

    Rename Page

--- src/app/auth/login.page.ts
+++ src/app/auth/login.page.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'login',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
+export class LoginPage {
   constructor() {}
 }
