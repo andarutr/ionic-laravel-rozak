76903f580ccc821e6b935eb0d2821f73bb174952
andarutr <andarutr@gmail.com>
2024-05-26 11:40:56 +0700

    Validasi login & register

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
     public function register(Request $req)
     {
-        // $this->validate($req, [
-        //     'nama_lengkap' => 'required',
-        //     'username' => 'required',
-        //     'password' => 'required'
-        // ]);
+        $this->validate($req, [
+            'nama_lengkap' => 'required',
+            'username' => 'required',
+            'password' => 'required'
+        ]);
 
         User::create([
             'nama_lengkap' => $req->nama_lengkap,
@@ -23,9 +23,26 @@ class AuthController extends Controller
             'password' => \Hash::make($req->password),
         ]);
         
-        // $response = new Response('Success');
-        // $response->header('Access-Control-Allow-Origin', 'http://localhost:8100');
-        // return $response;
-        return response('Success')->header('Access-Control-Allow-Origin', 'http://localhost:8100');
+        return response()->json(['msg' => 'Berhasil Register!'])
+            ->header('Access-Control-Allow-Origin', 'http://localhost:8100');
+    }
+
+    public function login(Request $req)
+    {
+        $this->validate($req, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        $username = $req->username;
+        $password = $req->password;
+
+        if(Auth::attempt(['username' => $username, 'password' => $password])){
+            return response()->json(['msg' => 'Berhasil Login!'])
+            ->header('Access-Control-Allow-Origin', 'http://localhost:8100');
+        }else{
+            return response()->json(['msg' => 'Gagal Login!'], 400)
+            ->header('Access-Control-Allow-Origin', 'http://localhost:8100');
+        }
     }
 }
